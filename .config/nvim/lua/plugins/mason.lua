d1ea07e941488c0da2a96e9658e4aa29d346b93f
Hamish B <hmblair@stanford.edu>
2025-06-16 22:37:08 -0700

    Only load if neovim >= 0.11

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,22 +1,26 @@
-return {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
+if vim.fn.has("nvim-0.11") == 1 then
+    return {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
 
-    {
-	"mason-org/mason.nvim",
-        opts = {
-	    ensure_installed = {
-	        "clangd",
-	    },
+        {
+            "mason-org/mason.nvim",
+            opts = {
+                ensure_installed = {
+                    "clangd",
+                },
+            },
         },
-    },
 
-    {
-        "neovim/nvim-lspconfig",
-        config = function() 
-            require("config.lspconfig")
-        end
+        {
+            "neovim/nvim-lspconfig",
+            config = function() 
+                require("config.lspconfig")
+            end
+        }
+
+        },
     }
-
-    },
-}
+else
+    return nil
+end
