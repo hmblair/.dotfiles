d64b8e08968c9e8b3252049715ee1783dbd7519d
Hamish B <hmblair@stanford.edu>
2025-07-08 15:05:11 -0700

    Removed old plugins; updated treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,34 @@
 if vim.fn.has("nvim-0.10") == 1 then
   return {
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'master', 
-    ensure_installed = {
-      "bash",
-      "c",
-      "cpp",
-      "json",
-      "latex",
-      "lua",
-      "markdown",
-      "python",
-      "yaml",
-      "javascript",
-      "cmake",
-      "json5",
-    },
-    lazy = false,
-    build = ":TSUpdate",
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    }
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    config = function ()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "cpp",
+          "json",
+          "latex",
+          "lua",
+          "markdown",
+          "python",
+          "yaml",
+          "javascript",
+          "cmake",
+          "json5",
+        },
+        lazy = false,
+        sync_install = false,
+        auto_install = true,
+        build = ":TSUpdate",
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end,
+  }
 else
   return {}
 end
